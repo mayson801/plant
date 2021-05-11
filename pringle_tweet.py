331fea8124d61fca896de7f4e9032dcf96982068
mayson801 <48494078+mayson801@users.noreply.github.com>
2021-05-11 18:08:43 +0100

    add AWS database intergration

--- pringle_tweet.py
+++ pringle_tweet.py
@@ -4,7 +4,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 from bs4 import BeautifulSoup
+import boto3
 import tweeter
+from datetime import date
 
 
 def close(self):
@@ -21,10 +23,11 @@ def close(self):
             except Exception as e:
                 print(e)
 
-def get_prices(shop_name,url, element_type, class_value,):
+def get_prices(shop_name,url, element_type, class_value,headless=True):
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+    if headless == True:
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')  # Last I checked this was necessary.
     driver = webdriver.Chrome('./chromedriver.exe', chrome_options=options)
     driver.get(url)
 
@@ -39,21 +42,32 @@ def get_prices(shop_name,url, element_type, class_value,):
             find_price_text = find_price_element[0].text.strip()
 
             driver.close()
-            print(shop_name+" done")
+            print(shop_name+" done") 
             break
         except:
+            print(soup)
             print("try again")
     return shop_name,find_price_text
 
 def get_all_super_markets():
-    tesco_price = get_prices("tesco","https://www.tesco.com/groceries/en-GB/products/296734865","span","value")
+    tesco_price = get_prices("tesco","https://www.tesco.com/groceries/en-GB/products/296734865","span","value",False)
     asda_price = get_prices("asda","https://groceries.asda.com/product/pringles-tube-snacks/pringles-original-sharing-crisps/910003062100","strong","co-product__price pdp-main-details__price")
     morrisons_price = get_prices("morrisons","https://groceries.morrisons.com/products/pringles-original-372817011", "h2", "bop-price__current")
     sainsburys_price = get_prices("sainsburys","https://www.sainsburys.co.uk/gol-ui/product/pringles-original-190g","div","pd__cost__total undefined")
     coop_price = get_prices("coop","https://www.coop.co.uk/products/pringles-original-200g","p","coop-c-card__price")
 
     list_of_shop_prices=[tesco_price,asda_price,morrisons_price,sainsburys_price,coop_price]
+
     return list_of_shop_prices
+def add_iteam(shop,price,date):
+    dynamodb = boto3.resource('dynamodb',region_name='eu-west-2')
+    table = dynamodb.Table('pringle_prices')
+    table.put_item(
+    Item= {
+        'date': date,
+        'shop': shop,
+        'price': price
+    })
 
 def create_pringal_tweet():
     #change_these
@@ -71,4 +85,13 @@ def create_pringal_tweet():
     with open('temp.txt', 'r') as f:
         #tweeter.tweet(f.read(),CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
         print(f.read())
-create_pringal_tweet()
+    os.remove("temp.txt")
+#create_pringal_tweet()
+data = get_all_super_markets()
+i=0
+date = date.today().strftime("%d/%m/%Y")
+while i < len(data):
+    shop=data[i][0]
+    price=data[i][1]
+    add_iteam(shop,price,date)
+    i=i+1
