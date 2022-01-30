22c16f267f99f3f7eb892360f422392a59768ede
MatthewMayson <mayson801@hotmail.com>
2022-01-30 18:06:13 +0000

    update 2022

--- pringle_tweet.py
+++ pringle_tweet.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 from bs4 import BeautifulSoup
-import boto3
 import tweeter
 from datetime import date
 
@@ -25,6 +24,7 @@ def close(self):
 
 def get_prices(shop_name,url, element_type, class_value,headless=True):
     options = Options()
+    failcount=0
     if headless == True:
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')  # Last I checked this was necessary.
@@ -42,18 +42,23 @@ def get_prices(shop_name,url, element_type, class_value,headless=True):
             find_price_text = find_price_element[0].text.strip()
 
             driver.close()
-            print(shop_name+" done") 
+            print(shop_name+" done")
             break
         except:
+            failcount=failcount+1
             print(soup)
             print("try again")
+            if failcount>10:
+                find_price_text="null"
+                driver.close()
+                break
     return shop_name,find_price_text
 
 def get_all_super_markets():
     tesco_price = get_prices("tesco","https://www.tesco.com/groceries/en-GB/products/296734865","span","value",False)
     asda_price = get_prices("asda","https://groceries.asda.com/product/pringles-tube-snacks/pringles-original-sharing-crisps/910003062100","strong","co-product__price pdp-main-details__price")
     morrisons_price = get_prices("morrisons","https://groceries.morrisons.com/products/pringles-original-372817011", "h2", "bop-price__current")
-    sainsburys_price = get_prices("sainsburys","https://www.sainsburys.co.uk/gol-ui/product/pringles-original-190g","div","pd__cost__total undefined")
+    sainsburys_price = get_prices("sainsburys","https://www.sainsburys.co.uk/gol-ui/product/pringles-original-190g","div","pd__cost__total undefined",False)
     coop_price = get_prices("coop","https://www.coop.co.uk/products/pringles-original-200g","p","coop-c-card__price")
 
     list_of_shop_prices=[tesco_price,asda_price,morrisons_price,sainsburys_price,coop_price]
@@ -71,10 +76,10 @@ def add_iteam(shop,price,date):
 
 def create_pringal_tweet():
     #change_these
-    CONSUMER_KEY = '###############'
-    CONSUMER_SECRET = '##################'
-    ACCESS_TOKEN = '#################'
-    ACCESS_TOKEN_SECRET = '#############'
+    CONSUMER_KEY =  '##############'
+    CONSUMER_SECRET = '####################'
+    ACCESS_TOKEN = '######################'
+    ACCESS_TOKEN_SECRET = '###################'
     list_of_shop_prices = get_all_super_markets()
     with open('temp.txt', 'w') as f:
         for shop in list_of_shop_prices:
@@ -83,15 +88,16 @@ def create_pringal_tweet():
           else:
                     f.write(shop[0] +" £" + shop[1]+ "\n")
     with open('temp.txt', 'r') as f:
-        #tweeter.tweet(f.read(),CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+        tweeter.tweet(f.read(),CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
         print(f.read())
     os.remove("temp.txt")
-#create_pringal_tweet()
-data = get_all_super_markets()
-i=0
-date = date.today().strftime("%d/%m/%Y")
-while i < len(data):
-    shop=data[i][0]
-    price=data[i][1]
-    add_iteam(shop,price,date)
-    i=i+1
+create_pringal_tweet()
+#data = get_all_super_markets()
+#print(data)
+#i=0
+#date = date.today().strftime("%d/%m/%Y")
+#while i < len(data):
+ #   shop=data[i][0]
+  #  price=data[i][1]
+   # #add_iteam(shop,price,date)
+    #i=i+1
--- schedule_tweets.py
+++ schedule_tweets.py
@@ -4,8 +4,8 @@ from plant_tweet import *
 from  pringle_tweet import *
 
 if __name__ == '__main__':
-    schedule.every().day.at("07:00").do(water_tweet)
-    schedule.every().day.at("19:00").do(water_tweet)
+    #schedule.every().day.at("07:00").do(water_tweet)
+    #schedule.every().day.at("19:00").do(water_tweet)
 
     schedule.every().day.at("08:00").do(create_pringal_tweet)
     while True:
